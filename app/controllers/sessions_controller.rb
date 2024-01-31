5e1d791b906ae0bb997fd18ee1b2577a9ca62932
kai-kondo <67088325+kai-kondo@users.noreply.github.com>
2024-01-31 10:57:48 +0900

    ログイン実行部分の開発

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    admin_user = AdminUser.find_by(email: session_params[:email])
+
+    if admin_user&authenticate(session_params[:password])
+      session[:admin_user_id] = admin_user.id
+      redirect_to root_url, notice: 'ログインしました'
+    else
+      render :new
+    end
+  end
+  private
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
+  post '/login', to: 'session#create'
+  
   namespace :admin do
     resources :admin_users
   end
