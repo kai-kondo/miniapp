74027e7664abe21a83f1a65a7ee7630745e3d072
kai-kondo <67088325+kai-kondo@users.noreply.github.com>
2024-01-31 09:56:30 +0900

    password_digestカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_28_032033) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_31_004912) do
   create_table "admin_users", charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "plan_id"
+    t.string "password_digest", null: false
   end
 
   create_table "plans", charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
