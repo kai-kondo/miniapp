4ab7106d335c56b3d39a7d5a12db571c9caeadb4
kai-kondo <67088325+kai-kondo@users.noreply.github.com>
2024-02-01 11:04:37 +0900

    モデル間の関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
   belongs_to :admin_user, foreign_key: "admin_user_id"
+  belongs_to :plan
+
+  validate :check_registration_limit, on: :created
+
+  private
+
+  def check_registration_limit
+    if admin_user.present? && plan.present?
+      max_users = plan.max_users
+
+      if admin_user.users.count >= max_users
+        errors.add(:base, "ユーザー登録は制限されています。")
+      end
+    end
+  end
 end
