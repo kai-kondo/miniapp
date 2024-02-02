0bcace6b3d5e39224ee7b7305ff032106b951209
kai-kondo <67088325+kai-kondo@users.noreply.github.com>
2024-02-02 17:40:35 +0900

    ユーザー登録制限を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
-    
+
   end
 
   # GET /users/new
@@ -32,15 +32,18 @@ class UsersController < ApplicationController
     @user.admin_user = @admin_user if @admin_user
 
     respond_to do |format|
+    ActiveRecord::Base.transaction do
       if @user.save
         format.html { redirect_to user_url(@user), notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
+        raise ActiveRecord::Rollback
       end
     end
   end
+end
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ApplicationRecord
   belongs_to :admin_user, foreign_key: "admin_user_id"
-
   validate :check_registration_limit, on: :created
+  before_validation :check_user_limit
 
   private
 
-  def check_registration_limit
-    if admin_user.present? && plan.present?
-      max_users = plan.max_users
+  def check_user_limit
+    if admin_user.present? && admin_user.plan.present?
+      max_users = admin_user.plan.max_users
 
       if admin_user.users.count >= max_users
         errors.add(:base, "ユーザー登録は制限されています。")
-      end
+        end
     end
   end
 end
